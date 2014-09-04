a0e180ae130644ddf46836ac9e42a9f8ec03a86d
engstk <eng.stk@sapo.pt>
2014-09-04 15:20:34 +0100

    dt2w: Option to disable with power button
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -32,6 +32,10 @@
 #include <linux/input/doubletap2wake.h>
 #endif
 
+#ifdef CONFIG_PWRKEY_SUSPEND
+#include <linux/qpnp/power-on.h>
+#endif
+
 #include "mdss_dsi.h"
 
 #define DT_CMD_HDR 6
@@ -187,6 +191,11 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 	if (prevent_sleep && in_phone_call)
 		prevent_sleep = false;
 	#endif
+	
+	#ifdef CONFIG_PWRKEY_SUSPEND
+	if (pwrkey_pressed)
+		prevent_sleep = false;
+	#endif
 
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
@@ -319,6 +328,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	if (local_pdata->on_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &local_pdata->on_cmds);
 
+	#ifdef CONFIG_PWRKEY_SUSPEND
+	pwrkey_pressed = false;
+	#endif
+	
 	pr_info("%s\n", __func__);
 	return 0;
 }
@@ -334,6 +347,11 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		prevent_sleep = false;
 	#endif
 
+	#ifdef CONFIG_PWRKEY_SUSPEND
+	if (pwrkey_pressed)
+		prevent_sleep = false;
+	#endif
+	
 	if (pdata == NULL) {
 		pr_err("%s: Invalid input data\n", __func__);
 		return -EINVAL;
