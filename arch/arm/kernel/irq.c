3d25005e76f0e64a8530b5e40c9ced4027e0e615
myfluxi <linflux@arcor.de>
2015-07-28 21:21:59 +0200

    arm: irq: Tone down kernel logging
    Change-Id: Ie31190bd65d1379de41e1ca2573aa65e785de1b6
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- arch/arm/kernel/irq.c
+++ arch/arm/kernel/irq.c
@@ -192,7 +192,7 @@ void migrate_irqs(void)
 
 /*
 		if (affinity_broken && printk_ratelimit())
-			pr_warning("IRQ%u no longer affine to CPU%u\n", i,
+			pr_debug("IRQ%u no longer affine to CPU%u\n", i,
 				smp_processor_id());
 */
 	}
